54fce6cdc1a1e850966c873ce44158dfad42e08b
Sravanthi <60406770+SravanthiMalepati@users.noreply.github.com>
2021-03-17 12:25:02 -0400

    First Commit

---
+++ app.py
@@ -0,0 +1,40 @@
+import os
+import pandas as pd
+from dash import Dash, callback_context, no_update
+from dash.dependencies import Input, Output
+from dash_table import DataTable
+import dash_core_components as dcc
+import dash_html_components as html
+import plot_final as plot
+
+path = "./data/sample_tennis_data_table.csv"
+df = pd.read_csv(path)
+
+app = Dash(__name__)
+app.layout = html.Div(
+    [
+        dcc.Graph(id='graph'),
+        dcc.Location(id='url',refresh=False)
+        # DataTable(
+        #     id="table",
+        #     columns=[{"name": i, "id": i} for i in df.columns],
+        #     data=df.to_dict("records"),
+        #     export_format="csv",
+        # )
+    ]
+)
+@app.callback(Output('graph', 'figure'),
+              [Input('url', 'pathname')])
+def display_page(pathname):
+    df = pd.read_csv(f'./data{pathname}.csv')
+
+    #return df.to_dict("records"), [{"name": i, "id": i} for i in df.columns]
+    #print('sravanthi')
+    fig = plot.plotly(df)
+    return fig
+
+
+
+if __name__ == "__main__":
+    app.run_server(host='127.0.0.1',debug=True)
+    #app.run_server(host='127.0.0.1', port=8080, debug=True)
